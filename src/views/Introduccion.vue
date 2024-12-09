46e53ae2d3bb612a3a9d748fe232e2f686544b82
rafperdevops <rafperdevops@gmail.com>
2024-12-09 08:40:34 -0500

    Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -16,7 +16,7 @@
       .col-lg-8
         p El componente formativo, Promoción de productos y servicios e informes de servicio. En el campo del Manejo de Información Turística, la promoción de productos y servicios se conecta estrechamente con los componentes de la oferta turística y los tipos de prestadores. En esta etapa del proceso formativo, el aprendiz ha investigado las necesidades del cliente y los atractivos disponibles, preparándose para analizar y construir estrategias de promoción integrales. Los informes de servicio juegan un papel crucial al verificar la precisión de la información y ajustar las estrategias según la satisfacción del usuario y las características de cada región. <b>Bienvenido a este componente formativo:</b>
     .video
-      iframe(width="560" height="315" src="https://www.youtube.com" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      iframe(width="560" height="315" src="https://www.youtube.com/embed/3KqxUXy6hfQ?si=TddbICzXG8hwpCzT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     figcaption Nota: Sena (2024).   
     
 </template>
