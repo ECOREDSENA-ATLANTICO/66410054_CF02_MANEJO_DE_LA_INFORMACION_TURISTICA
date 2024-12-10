24b1b0f540cd484a45fbe211ff1f266e6dc260bc
rafperdevops <rafperdevops@gmail.com>
2024-12-10 12:13:27 -0500

    PDF

--- src/config/global.js
+++ src/config/global.js
@@ -79,7 +79,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/66410054_CF02_DU.pdf',
       },
       {
         icono: 'fas fa-download',
